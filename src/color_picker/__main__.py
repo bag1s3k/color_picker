82d8172ea7d68d0ad03781d2809c0092fe9fd610
bag1s3k <jetam.mravenec@gmail.com>
2026-05-17 19:57:35 +0200

    Display input data in preview labels

--- src/color_picker/__main__.py
+++ src/color_picker/__main__.py
@@ -1,14 +1,14 @@
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import RadioButton, RadioSet, Static, Footer, Header, Input, Label
-from textual.containers import Horizontal, Grid, Container
+from textual.widgets import Input, RadioSet, Label, Header, Footer
+from textual.containers import Container, Horizontal
+from textual.reactive import reactive
 from textual.binding import Binding
 from textual.events import Resize
-from textual.reactive import reactive
 
 from color_picker.constants import COLOR_SPACES
-from color_picker.widgets import PyfigletText
-from color_picker.help import circle_buffer, input_formatted_string
+from color_picker.help import input_formatted_string, circle_buffer
+from color_picker.widgets import PyfigletText, Inputs, SelectColorSpace, ColorPreview
 
 
 class ColorPicker(App[None]):
@@ -22,80 +22,80 @@ class ColorPicker(App[None]):
     ]
 
     selected_space = reactive("RGB")
+    channels = reactive([0, 0, 0])
 
     def compose(self) -> ComposeResult:
         yield Header(icon="☰")
-
         yield PyfigletText("C o l o r  P i c k e r")
 
-        # Static widget with color + buttons to choose color space
-        with Horizontal(id="top-part"):
-            with Container(id="color-container"):
-                yield Static()
-
-            radio_set = RadioSet()
-            radio_set.border_subtitle = "Color Spaces"
-            with radio_set:
-                for color_space in COLOR_SPACES.keys():
-                    yield RadioButton(color_space, value=True)
-
-        # Inputs
-        with Horizontal(id="inputs"):
-            formatted_string = input_formatted_string(self.selected_space)
-
-            for i in range(len(COLOR_SPACES[self.selected_space]["channels"])):
-                yield Input(placeholder=next(formatted_string), id=f"input-{i}")
-
-        # Displaying colors in other spaces + aesthetic RGB ASCII image
-        with Horizontal(id="bottom-part"):
-            with Grid():
-                for color_space, specs in COLOR_SPACES.items():
-                    i = circle_buffer()
-                    unit = specs["unit"]
-
-                    label = Label(
-                        f"0{unit[next(i)]} 0{unit[next(i)]} 0{unit[next(i)]}",
-                        id=f"label-{color_space}",
-                    )
-                    label.border_title = color_space
-                    yield label
-
-            yield PyfigletText("#ffffff")
+        yield SelectColorSpace(id="select-space-panel")
+        yield Inputs(id="inputs-panel")
+        yield ColorPreview(id="preview-panel")
 
         yield Footer()
 
     @on(RadioSet.Changed)
-    def radioset_changed(self, event: RadioSet.Changed) -> None:
+    def handle_radio_change(self, event: RadioSet.Changed) -> None:
+        """Update selected color space"""
         self.selected_space = str(event.pressed.label)
 
+    @on(Input.Changed)
+    def handle_input_change(self, event: Input.Changed) -> None:
+        """Update channels according to input"""
+        i = int(str(event.input.id)[-1])
+        self.channels[i] = int(event.value)
+        self.mutate_reactive(ColorPicker.channels)
+
     def watch_selected_space(self, old_space: str, new_space: str) -> None:
+        """Update Input's placeholder and highlight of preview according to selected space"""
+        if not self.is_mounted:
+            return
+
         try:
             self.query_one(f"#label-{old_space}", Label).remove_class("highlight")
-            self.query_one(f"#label-{new_space}", Label).add_class("highlight")
         except Exception:
             pass
+        self.query_one(f"#label-{new_space}", Label).add_class("highlight")
 
-        formatted_string = input_formatted_string(self.selected_space)
-        for i in range(len(COLOR_SPACES[self.selected_space]["channels"])):
+        formatted_string = input_formatted_string(new_space)
+        for i in range(len(COLOR_SPACES[new_space]["channels"])):
+            text_input = self.query_one(f"#input-{i}", Input)
+            text_input.placeholder = str(next(formatted_string))
+
+    def watch_channels(self, new_channels: list) -> None:
+        """Update preview Labels according to input"""
+        if not self.is_mounted:
+            return
+
+        for color_space, specs in COLOR_SPACES.items():
+            i = circle_buffer()
+            unit = specs["unit"]
+
+            new_text = f"{new_channels[0]}{unit[next(i)]} {new_channels[1]}{unit[next(i)]} {new_channels[2]}{unit[next(i)]}"
+
             try:
-                text_input = self.query_one(f"#input-{i}", Input)
-                text_input.placeholder = str(next(formatted_string))
+                label = self.query_one(f"#label-{color_space}", Label)
+                label.update(new_text)
             except Exception:
                 pass
 
     def on_mount(self) -> None:
         self.title = "Color Picker"
         self.sub_title = "Support five different color spaces"
-        self.query_one(f"#label-{self.selected_space}", Label).add_class("highlight")
+
+        self.watch_selected_space("", self.selected_space)
+        self.watch_channels(self.channels)
 
     def on_resize(self, event: Resize) -> None:
-        square = self.query_one("Horizontal > #color-container", Container)
-        horizontal = self.query_one("Horizontal", Horizontal)
+        try:
+            square = self.query_one("#color-container", Container)
+            horizontal = self.query_one("#select-space-panel", Horizontal)
 
-        term_base_width = int(event.size.width * 0.06)
-
-        square.styles.width = term_base_width * 2
-        horizontal.styles.height = term_base_width
+            term_base_width = int(event.size.width * 0.06)
+            square.styles.width = term_base_width * 2
+            horizontal.styles.height = term_base_width
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
--- src/color_picker/widgets.py
+++ src/color_picker/widgets.py
@@ -1,5 +1,9 @@
 import pyfiglet
-from textual.widgets import Static
+from textual.widgets import Static, Input, RadioSet, RadioButton, Label
+from textual.containers import Horizontal, Container, Grid
+from textual.app import ComposeResult
+
+from color_picker.constants import COLOR_SPACES
 
 
 class PyfigletText(Static):
@@ -8,3 +12,38 @@ class PyfigletText(Static):
     def __init__(self, text: str, font: str = "standard", **kwargs):
         rendered_text = pyfiglet.figlet_format(text, font=font).rstrip("\n")
         super().__init__(rendered_text, **kwargs)
+
+
+class Inputs(Horizontal):
+    """Represents Horizontal widget with Inputs"""
+
+    def compose(self) -> ComposeResult:
+        for i in range(3):
+            yield Input(id=f"input-{i}")
+
+
+class SelectColorSpace(Horizontal):
+    """Represents Horizontal widget with RadioSet + square with color"""
+
+    def compose(self) -> ComposeResult:
+        with Container(id="color-container"):
+            yield Static()
+
+        radio_set = RadioSet()
+        radio_set.border_subtitle = "Color Spaces"
+        with radio_set:
+            for color_space in COLOR_SPACES.keys():
+                yield RadioButton(color_space, value=True)
+
+
+class ColorPreview(Horizontal):
+    """Represents Horizontal widget with color preview + aesthetic RGB ASCII image"""
+
+    def compose(self) -> ComposeResult:
+        with Grid():
+            for color_space in COLOR_SPACES.keys():
+                label = Label("", id=f"label-{color_space}")
+                label.border_title = color_space
+                yield label
+
+        yield PyfigletText("#ffffff", id="hex-ascii")
